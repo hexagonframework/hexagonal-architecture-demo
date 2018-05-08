216b3cdf60e5d90f9a34ddef133ee20a50ac09bc
yuanxuegui <yuanxuegui@163.com>
2018-05-08 11:51:41 +0800

    init rest service

---
+++ hexagonal-architecture-demo-adapter/hexagonal-architecture-demo-adapter-rest/hexagonal-architecture-demo-adapter-rest-service/src/main/java/io/github/hexagonframework/demo/rest/service/model/Greeting.java
@@ -0,0 +1,19 @@
+package io.github.hexagonframework.demo.rest.service.model;
+
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
